ecae887c5e15a23202ccaf51f9a998772c0f2a83
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-16 21:21:32 +0300

    Write test for rendering

---
+++ src/components/Repos/__tests__/Repos.test.tsx
@@ -0,0 +1,42 @@
+import { screen, render } from '@testing-library/react';
+import Repos from '../Repos';
+import { IRepo } from '../types';
+
+describe('Rendering data', () => {
+  const mockRepos: IRepo[] = [
+    {
+      name: 'test1',
+      description: 'desc1',
+      htmlUrl: '/',
+      id: '1',
+    },
+    {
+      name: 'test2',
+      description: 'desc2',
+      htmlUrl: '/',
+      id: '2',
+    },
+    {
+      name: 'test3',
+      description: 'desc3',
+      htmlUrl: '/',
+      id: '3',
+    },
+    {
+      name: 'test4',
+      description: 'desc4',
+      htmlUrl: '/',
+      id: '4',
+    },
+  ];
+
+  it('Renders repos when they are provided', () => {
+    render(<Repos repos={mockRepos} />);
+
+    mockRepos.forEach((repo) => {
+      expect(screen.getByText(repo.name)).toBeInTheDocument();
+      expect(screen.getByText(repo.description)).toBeInTheDocument();
+    });
+  });
+
+});
