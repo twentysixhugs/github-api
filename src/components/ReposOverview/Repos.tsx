f8e15992bbd1f6322a94cad327a5bb8a471b3555
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-17 20:50:52 +0300

    Build pagination

--- src/components/ReposOverview/Repos.tsx
+++ src/components/ReposOverview/Repos.tsx
@@ -6,34 +6,19 @@ type ReposProps = {
 };
 
 export default function Repos({ repos }: ReposProps) {
-  let viewToRender;
-
-  if (repos.length > 0) {
-    viewToRender = (
-      <>
-        <h1 className="c-repos__title">Repositories</h1>
-        {repos.map((repo) => {
-          return (
-            <ReposEntry
-              name={repo.name}
-              description={repo.description}
-              htmlUrl={repo.htmlUrl}
-              key={repo.id}
-            />
-          );
-        })}
-      </>
-    );
-  } else {
-    viewToRender = (
-      <>
-        <img src="" alt=""></img>
-        <span className="c-repos__message-empty">
-          Repository list is empty
-        </span>
-      </>
-    );
-  }
-
-  return <div className="c-repos">{viewToRender}</div>;
+  return (
+    <div className="c-repos">
+      <h1 className="c-repos__title">Repositories</h1>
+      {repos.map((repo) => {
+        return (
+          <ReposEntry
+            name={repo.name}
+            description={repo.description}
+            htmlUrl={repo.htmlUrl}
+            key={repo.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- src/components/ReposOverview/ReposOverview.tsx
+++ src/components/ReposOverview/ReposOverview.tsx
@@ -1,15 +1,78 @@
 import Repos from './Repos';
 import { IRepo } from './types';
+import ReactPaginate, { ReactPaginateProps } from 'react-paginate';
+import { useState, useEffect } from 'react';
 
 type ReposOverviewProps = {
   allRepos: IRepo[];
 };
 
 export default function ReposOverview({ allRepos }: ReposOverviewProps) {
-  // TODO: it should pass the determined number of items
-  return (
-    <div className="c-repos-overview">
-      <Repos repos={allRepos} />
-    </div>
-  );
+  const [currentItems, setCurrentItems] = useState<null | IRepo[]>(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+
+  // The number of items that have already been displayed by the previous pages
+  const [pageOffset, setPageOffset] = useState(0);
+
+  // How many items we want to show on each page
+  const PER_PAGE = 4;
+
+  useEffect(() => {
+    /* When the repos are updated, reset pagination to the initial state */
+    setCurrentPage(0);
+    setPageOffset(0);
+  }, [allRepos]);
+
+  useEffect(() => {
+    console.log(currentPage);
+    setPageOffset(PER_PAGE * currentPage);
+  }, [currentPage]);
+
+  useEffect(() => {
+    setCurrentItems(allRepos.slice(pageOffset, pageOffset + PER_PAGE));
+
+    setPageCount(Math.ceil(allRepos.length / PER_PAGE));
+  }, [pageOffset, allRepos]);
+
+  const handlePageClick = ({ selected }: { selected: number }) => {
+    setCurrentPage(selected);
+  };
+
+  let viewToRender;
+
+  if (allRepos.length > 0) {
+    viewToRender = (
+      <>
+        {currentItems && <Repos repos={currentItems} />}
+        <div className="c-repos-overview__wrapper c-repos-overview__wrapper--pagination">
+          <span className="c-repos-overview__pagination-stats">
+            {pageOffset + 1}-{pageOffset + PER_PAGE} of {allRepos.length}{' '}
+            items
+          </span>
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            pageCount={pageCount}
+            previousLabel="< previous"
+            renderOnZeroPageCount={() => null}
+            containerClassName="c-repos-overview__pagination-ui"
+            activeClassName="active-page"
+          />
+        </div>
+      </>
+    );
+  } else {
+    viewToRender = (
+      <>
+        <img src="" alt=""></img>
+        <span className="c-repos__message-empty">
+          Repository list is empty
+        </span>
+      </>
+    );
+  }
+  return <div className="c-repos-overview">{viewToRender}</div>;
 }
