da41978d8f95cdb85b1fd832ae9b990edb6cb50d
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-18 10:29:36 +0300

    Style initial screen and user not found state

--- src/components/SearchState/SearchState.tsx
+++ src/components/SearchState/SearchState.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Loader from './Loader';
 import searchIconPath from './assets/search_icon.svg';
 import userIconPath from './assets/user_icon.svg';
-import Loader from './Loader';
+import './SearchState.css';
 
 type SearchStateProps = {
   searchState?: 'Initial state' | 'Loading' | 'User not found' | 'Error';
@@ -16,14 +17,16 @@ export default function SearchState({
     case 'Initial state':
       return (
         <Base classNameModifier="initial">
-          <img
-            className="c-search-state__img"
-            src={searchIconPath}
-            alt=""
-          ></img>
-          <span className="c-search-state__message">
-            Start with searching a GitHub user
-          </span>
+          <div className="c-search-state__wrapper">
+            <img
+              className="c-search-state__img"
+              src={searchIconPath}
+              alt=""
+            ></img>
+            <span className="c-search-state__message">
+              Start with searching a GitHub user
+            </span>
+          </div>
         </Base>
       );
     case 'Loading':
@@ -35,21 +38,25 @@ export default function SearchState({
     case 'User not found':
       return (
         <Base classNameModifier="user-not-found">
-          <img
-            className="c-search-state__img"
-            src={userIconPath}
-            alt=""
-          ></img>
-          <span className="c-search-state__message">User not found</span>
+          <div className="c-search-state__wrapper">
+            <img
+              className="c-search-state__img"
+              src={userIconPath}
+              alt=""
+            ></img>
+            <span className="c-search-state__message">User not found</span>
+          </div>
         </Base>
       );
     case 'Error':
       /* For handling everything that's not 404 so that the app doesn't break */
       return (
         <Base classNameModifier="error">
-          <span className="c-search-state__message">
-            Something went wrong
-          </span>
+          <div className="c-search-state__wrapper">
+            <span className="c-search-state__message">
+              Something went wrong
+            </span>
+          </div>
         </Base>
       );
     default:
